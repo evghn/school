fc8411b3f9e1502524c69d96a2dd5b7f556f4e1e
evghn <evghn94_git@mail.ru>
2025-05-04 13:57:40 +0300

    created button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
-<template></template>
+<script setup>
+import Button from "./components/Button.vue";
+</script>
+<template>
+  <main class="main">
+    <Button> Начать игру </Button>
+  </main>
+</template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+.main {
+  display: grid;
+  place-items: center;
+  min-height: 100vh;
+  font-family: "Montserrat", sans-serif;
+  font-style: normal;
+  background-color: var(--bg-main);
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="btn">
+    <slot></slot>
+  </button>
+</template>
+<style scoped>
+.btn {
+  max-width: 335px;
+  min-width: 335px;
+  max-height: 68px;
+  min-height: 68px;
+  background-color: var(--bg-btn);
+  color: var(--font-btn);
+  font-size: 24px;
+  line-height: 36px;
+  font-weight: 400;
+  border: none;
+  border-radius: 100px;
+}
+</style>
